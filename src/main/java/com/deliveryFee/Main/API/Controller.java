815672194fb71b8288080b758cf951c0711a43e5
RobinMurk <murkrobin@gmail.com>
2025-03-24 02:30:36 +0200

    added basic tests and documentation for endpoints

--- src/main/java/com/deliveryFee/Main/API/Controller.java
+++ src/main/java/com/deliveryFee/Main/API/Controller.java
@@ -32,25 +32,72 @@ public class Controller {
         this.feeService = feeService;
     }
 
+
+    /**
+     * Basic GET endpoint for testing server response
+     * @return String "hello"
+     */
     @GetMapping("/hello")
     public String Test(){
         return "hello";
     }
 
+    /**
+     * POST endpoint for getting the delivery fee based on most recent
+     * Weather data
+     * @param request PostRequest Record <br>
+     *                request JSON format: <br>
+     *                { <br>
+     *                  "city" : CITY NAME HERE,<br>
+     *                  "vehicle" : VEHICLE NAME HERE <br>
+     *                } <br><br>
+     * <bold>Example JSON:</bold><br>
+     *                { <br>
+     *                   "city" : "Tallinn",<br>
+     *                   "vehicle" : "Car" <br>
+     *                } <br>
+     *
+     *
+     * @return PostResponse Record <br>
+     * <bold>Example JSON response:</bold> <br>
+     * { <br>
+     *   "fee" : "Tallinn",<br>
+     *   "statement" : "SUCCESS" <br>
+     * } <br>
+     * @throws APIException
+     */
     @PostMapping("/get-fee")
-    public ResponseEntity<PostResponse> getFee(@RequestBody PostRequest request) {
-        try {
+    public ResponseEntity<PostResponse> getFee(@RequestBody PostRequest request) throws APIException {
             double fee = feeService.calculateFee(request.city, request.vehicle);
             return new ResponseEntity<>(
                     new PostResponse(fee, "SUCCESS"), HttpStatus.OK
             );
-        }catch (APIException e){
-            return new ResponseEntity<>(
-                    new PostResponse(e.getValue(), e.getMessage()), HttpStatus.BAD_REQUEST
-            );
-        }
     }
 
+    /**
+     * Returns the Ruleset of all possible rules that can be manipulated through api/v1/update-rules endpoint
+     * @return RuleList Record <br>
+     * <br>
+     * <bold>Example JSON response:</bold><br>
+     * {
+     *     "rules": [
+     *         "TALLINN_CAR_RBF",
+     *         "TALLINN_SCOOTER_RBF",
+     *         "TALLINN_BIKE_RBF",
+     *         "TARTU_CAR_RBF",
+     *         "TARTU_SCOOTER_RBF",
+     *         "TARTU_BIKE_RBF",
+     *         "PARNU_CAR_RBF",
+     *         "PARNU_SCOOTER_RBF",
+     *         "PARNU_BIKE_RBF",
+     *         "EXTRA_FOR_RAIN",
+     *         "EXTRA_FOR_SNOW",
+     *         "EXTRA_FOR_WIND",
+     *         "EXTRA_FOR_LOWER_TEMP",
+     *         "EXTRA_FOR_LOW_TEMP"
+     *     ]
+     * }
+     */
     @GetMapping("/get-ruleset")
     public ResponseEntity<RuleList> getRuleset(){
         return new ResponseEntity<>(
@@ -58,20 +105,51 @@ public class Controller {
         );
     }
 
+    /**
+     * Updates all business rules with new values,
+     * @param request UpdateRuleResponse Record, Contains a list of Rule Records,<br>
+     *                each record has rule name and new value. <br>
+     *                Correct rule names can be acquired from GET /api/v1/get-ruleset endpoint<br>
+     *                or the getRuleset() method<br>
+     *                <bold>Example JSON request:</bold><br>
+     *                {
+     *              "ruleset": [ <br>
+     *                  { "rule": "TALLINN_CAR_RBF", "value": 5 },<br>
+     *                  { "rule": "EXTRA_FOR_SNOW", "value": 10 }<br>
+     *                  ]
+     *              }
+     * @return UpdateRuleResponse Record, that contains the rules that were successfully parsed.<br>
+     * NB: This implementation is faulty. THIS DOES NOT GUARANTEE that the returned rules were successfully changed<br>
+     * due to bad implementation <br>
+     * //TODO fix proper handling of exceptions <br>
+     * <bold>Example JSON response:</bold><br>
+     * {
+     *     "status": "SUCCESS",
+     *     "values": [
+     *         "TALLINN_CAR_RBF",
+     *         "EXTRA_FOR_SNOW"
+     *     ]
+     * }
+     * @throws APIException APIException with value of -1 and caught errror message
+     */
     @PostMapping("/update-rules")
-    public ResponseEntity<UpdateRuleResponse> updateRule(@RequestBody UpdateRuleRequest request) {
-        try {
+    public ResponseEntity<UpdateRuleResponse> updateRule(@RequestBody UpdateRuleRequest request) throws APIException {
             List<String> sucessfulValues = feeService.updateRules(request.ruleset());
             return new ResponseEntity<>(
                     new UpdateRuleResponse("SUCCESS",sucessfulValues), HttpStatus.OK
             );
-        }catch (APIException e){
-            return new ResponseEntity<>(
-                    new UpdateRuleResponse("error: " + e.getMessage(), List.of()), HttpStatus.INTERNAL_SERVER_ERROR
-            );
-        }
     }
 
+    /**
+     * Handles all Exception class exceptions
+     * @param e Exception class object
+     * @return ErrorResponse Record with 500 response code<br>
+     * <bold>Example:</bold><br>
+     * { <br>
+     *  "status" : "Unknown error",<br>
+     *  "vehicle" : "Cannot invoke \"String.matches(String)\" because \"rawInput\" is null" <br>
+     *} <br>
+     */
     @ExceptionHandler(Exception.class)
         public ResponseEntity<ErrorResponse> handleException(Exception e){
             return new ResponseEntity<>(
@@ -79,6 +157,22 @@ public class Controller {
             );
         }
 
+    /**
+     * Handles all APIExceptions
+     * @param e The caught APIException
+     * @return Error response with code 400 repsonse code<br>
+     * <bold>Example JSON response:</bold><br>
+     *{<br>
+     *     "status": "Unexpected value: Tallidadann",<br>
+     *     "Statement": "User Error"<br>
+     * }
+     */
+    @ExceptionHandler(APIException.class)
+    public ResponseEntity<ErrorResponse> handleAPIexception(APIException e) {
+        return new ResponseEntity<>(
+                new ErrorResponse(e.getMessage(), "User Error"), HttpStatus.BAD_REQUEST
+        );
+    }
 
     private ResponseEntity<PostResponse> createPostResponse(double value, String statement, HttpStatus status){
         return new ResponseEntity<>(
---
+++ src/test/java/com/deliveryFee/Main/MainTest.java
@@ -0,0 +1,80 @@
+package com.deliveryFee.Main;
+
+import com.deliveryFee.Main.API.Controller;
+import com.deliveryFee.Main.API.FeeService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.List;
+
+import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+@ExtendWith(MockitoExtension.class)
+public class MainTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Mock
+	private FeeService feeService;
+
+	@InjectMocks
+	private Controller controller;
+
+	@BeforeEach
+	public void setup() {
+		mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+	}
+
+	@Test
+	public void testGetRuleset() throws Exception {
+		String city = "Tallinn";
+		String vehicle = "Car";
+
+		lenient().when(feeService.getRules()).thenReturn(List.of(
+				"TALLINN_CAR_RBF",
+				"TALLINN_SCOOTER_RBF",
+				"TALLINN_BIKE_RBF",
+				"TARTU_CAR_RBF",
+				"TARTU_SCOOTER_RBF",
+				"TARTU_BIKE_RBF",
+				"PARNU_CAR_RBF",
+				"PARNU_SCOOTER_RBF",
+				"PARNU_BIKE_RBF",
+				"EXTRA_FOR_RAIN",
+				"EXTRA_FOR_SNOW",
+				"EXTRA_FOR_WIND",
+				"EXTRA_FOR_LOWER_TEMP",
+				"EXTRA_FOR_LOW_TEMP"
+		));
+
+		mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/get-ruleset"))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.jsonPath("$.rules").isArray());
+
+	}
+
+	@Test
+	public void testChangingRules() throws Exception {
+		List<String> rules = List.of("TARTU_CAR_RBF","PARNU_SCOOTER_RBF","EXTRA_FOR_LOW_TEMP");
+		mockMvc.perform(post("/api/v1/update-rules")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content("{\"ruleset\":[{\"rule\":\" TARTU_CAR_RBF\",\"value\": 5}]}")
+				)
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.jsonPath("$.status").value("SUCCESS"))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.values").isArray());
+	}
+}
